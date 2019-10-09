e36c49eb153ea31087360e7d1d515fed8ac061cb
James Ward <james@jamesward.com>
2019-10-09 10:46:42 -0600

    logging

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
   @RequestMapping("/")
   public String index(@AuthenticationPrincipal OidcUser user) {
+    logger.info(user.toString());
     return String.format("hello, %s", user.getFullName());
   }
 
